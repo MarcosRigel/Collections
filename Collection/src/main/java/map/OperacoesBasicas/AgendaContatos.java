d13ad32c2cc6c3c5833320bb8b04f1d9c79014c9
Marcos Rigel <marcosrigel99@gmail.com>
2023-08-06 23:38:15 -0300

    Operacoes basicas map

---
+++ Collection/src/main/java/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,35 @@
+package main.java.map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+	
+	private Map<String, Integer> agendaContatoMap;
+
+	public AgendaContatos(Map<String, Integer> agendaContatoMap) {
+		this.agendaContatoMap = new HashMap<>();
+	}
+	
+	public void adicionarContato(String nome, Integer telefone) {
+		agendaContatoMap.put(nome, telefone);
+	}
+	
+	public void removerContato(String nome) {
+		if(!agendaContatoMap.isEmpty()) {
+			agendaContatoMap.remove(nome);
+		}
+	}
+	
+	public void exibirContatos() {
+		System.out.println(agendaContatoMap);
+	}
+	
+	public Integer pesquisarPorNome(String nome) {
+		Integer numeroPorNome = null;
+		if (agendaContatoMap.isEmpty()) {
+			numeroPorNome = agendaContatoMap.get(nome);
+		}
+		return numeroPorNome;
+	}
+}
